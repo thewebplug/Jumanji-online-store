9575b84812e98542e07cc86eb747e18d282477c6
Saleem Jibril <saleemjibril5@gmail.com>
2022-04-12 02:17:34 +0100

    WAGMI removed from upload router

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import { isAuth, isAdmin } from '../utils';
-// WAGMI
+
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, 'uploads/');
