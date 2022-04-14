5214198859b36b2fcdfc50447a63836ab7117f49
Saleem Jibril <saleemjibril5@gmail.com>
2022-04-14 02:13:04 +0100

    almost back to normal

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,7 @@ app.get('/api/paypal/clientId', (req, res) => {
   res.send({clientId: config.PAYPAL_CLIENT_ID});
 });
 app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
-app.use(express.static(path.join(__dirname, '/../frontend')));
-app.get('*',(req, res) => {
-  res.sendFile(path.join(__dirname, '/../frontend/index.html'));
-});
+
 app.use((err, req, res, next) => {
   const status = err.name && err.name === 'ValidationError' ? 400 : 500;
   res.status(status).send({ message: err.message });
