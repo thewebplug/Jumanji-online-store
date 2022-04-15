b642df5b09a0f916240f734493e861bc14cccfc7
Saleem Jibril <saleemjibril5@gmail.com>
2022-04-15 23:59:02 +0100

    stopped '%20' from showing in the search bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,8 @@
 import { getUserInfo } from '../localStorage';
-import { parseRequestUrl } from '../utils';
 
 const Header = {
   render: () => {
     const { name, isAdmin } = getUserInfo();
-    const { value } = parseRequestUrl();
     return ` 
   <button id="aside-open-button">
       <p>&#9776;</p>
@@ -14,16 +12,12 @@ const Header = {
   </div>
   <div class="search">
   <form class="search-form"  id="search-form">
-    <input type="text" name="q" id="q" value="${value || ''}" /> 
+    <input type="text" name="q" id="q" value="" /> 
     <button  type="submit"><i class="fa fa-search"></i></button>
   </form>        
   </div>
   <div class="sign-in">
-    ${
-      name
-        ? `<a class="profile-icon" href="/#/profile"><i class="fas fa-user-alt"></i></a>`
-        : `<a class="profile" href="/#/signin">Sign-In</a>`
-    }    
+    ${name ? `<a class="profile-icon" href="/#/profile"><i class="fas fa-user-alt"></i></a>` : `<a class="profile" href="/#/signin">Sign-In</a>`}    
       <a class="cart-icon" href="/#/cart">
       <i class="fal fa fa-shopping-cart"></i></a>
       ${isAdmin ? `<a class="dashboard-icon" href="/#/dashboard">Dashboard</a>` : ''}
